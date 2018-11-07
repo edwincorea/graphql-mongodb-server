eedf4f7416a557d540fa654fb784fba583f97ade
Edwin Corea Bonilla <ecorea@cruxconsultores.com>
2018-11-07 15:21:35 -0600

    Express setup.

--- Index.js
+++ Index.js
@@ -1,21 +1,47 @@
-import express from "express"
-import expressGraphQL from "express-graphql"
-import mongoose from "mongoose"
-import bodyParser from "body-parser"
-import cors from "cors"
+import express from 'express'
+import expressGraphQL from 'express-graphql'
+import mongoose from 'mongoose'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import path from 'path'
 
 const app = express()
-const PORT = process.env.PORT || "4000"
-const db = "mongodb://<dbuser>:<dbpassword>@ds155073.mlab.com:55073/graphql-mongodb-server"
+const PORT = process.env.PORT || '4000'
+
+const db =
+    'mongodb://ecorea:P4$$w0rd@ds155073.mlab.com:55073/graphql-mongodb-server'
+const schema = ''
 
 // Connect to MongoDB with Mongoose.
 mongoose
-  .connect(
-	  db,
-	  {
-		  useCreateIndex: true,
-		  useNewUrlParser: true
-	  }
-  )
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.log(err));
+    .connect(
+        db,
+        {
+            useCreateIndex: true,
+            useNewUrlParser: true,
+        },
+    )
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err))
+
+app.use(
+    '/graphql',
+    cors(),
+    bodyParser.json(),
+    expressGraphQL({
+        schema,
+        graphiql: true,
+    }),
+)
+
+// Serve static assets if in production.
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder.
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
--- README.md
+++ README.md
@@ -1,24 +1,34 @@
 # 🌐 A GraphQL MongoDB server.
 
 > A simple GraphQL MongoDB server, using Express, Babel, Mongoose and just that.
-> Feel free to clone the repository, submit changes and so on. 
+> Feel free to clone the repository, submit changes and so on.
 
 ### Getting Started
 
 Run just the server on https://localhost:4000/graphql:
 
 ```sh
-$ npm install
-$ npm run server
+$ npm install
+$ npm run server
+```
+
+Run
 ```
 
-Run the client on https://localhost:3000/ and Server on https://localhost:4000/graphql:
+Run the client on tall
+$ npm run dev
+ and Server on ```sh
+$ npm install
+$ npm r:
 
 ```sh
-$ npm install
-$ npm run dev
+```
+
+### License
+
+MIT
 ```
 
 ### License
 
-MIT (c) Leonardo Maldonado ([@leonardomso](https://twitter.com/leonardomso))
+MIT (c) Leonardo Maldonado (
