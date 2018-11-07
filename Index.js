065d54dcdc5e25cc68ac7c36655a04662550ce01
Edwin Corea Bonilla <ecorea@cruxconsultores.com>
2018-11-07 15:25:44 -0600

    MongoDB and Schema.

--- Index.js
+++ Index.js
@@ -9,39 +9,39 @@ const app = express()
 const PORT = process.env.PORT || '4000'
 
 const db =
-    'mongodb://ecorea:P4$$w0rd@ds155073.mlab.com:55073/graphql-mongodb-server'
+	'mongodb://ecorea:P4$$w0rd@ds155073.mlab.com:55073/graphql-mongodb-server'
 const schema = ''
 
 // Connect to MongoDB with Mongoose.
 mongoose
-    .connect(
-        db,
-        {
-            useCreateIndex: true,
-            useNewUrlParser: true,
-        },
-    )
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.log(err))
+	.connect(
+		db,
+		{
+			useCreateIndex: true,
+			useNewUrlParser: true,
+		},
+	)
+	.then(() => console.log('MongoDB connected'))
+	.catch(err => console.log(err))
 
 app.use(
-    '/graphql',
-    cors(),
-    bodyParser.json(),
-    expressGraphQL({
-        schema,
-        graphiql: true,
-    }),
+	'/graphql',
+	cors(),
+	bodyParser.json(),
+	expressGraphQL({
+		schema,
+		graphiql: true,
+	}),
 )
 
 // Serve static assets if in production.
 if (process.env.NODE_ENV === 'production') {
-    // Set static folder.
-    app.use(express.static('client/build'))
+	// Set static folder.
+	app.use(express.static('client/build'))
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	})
 }
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
